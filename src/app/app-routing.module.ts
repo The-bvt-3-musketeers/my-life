b4f6be1ae2262949581e7f4fbc3987ee8e9d9f3f
Jorge Jauregui Gallo <jorgejaureguigallo@gmail.com>
2020-11-19 18:59:38 -0800

    more configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { CalendarComponent } from './calendar/calendar.component';
 import { NotepadComponent } from './notepad/notepad.component';
 import { WeatherComponent } from './weather/weather.component';
 import { HomeComponent } from './home/home.component';
+import { LauraComponent } from './laura/laura.component';
+
 
 
 const routes: Routes = [
@@ -11,6 +13,8 @@ const routes: Routes = [
   { path: 'calendar', component: CalendarComponent },
   { path: 'notepad', component: NotepadComponent },
   { path: 'weather', component: WeatherComponent },
+  { path: 'lauras-component', component: LauraComponent },
+
 
 ];
 
